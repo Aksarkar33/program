216f1c4ffa36ca99405610c333d8ea76807a5690
Aksarkar33 <aksarkar@gmail.com>
2025-07-10 23:01:33 +0530

    Initial commit

---
+++ SmallInt.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class SmallInt {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        int x=sc.nextInt();
+     
+        int[] arr =new int[x];
+
+        for(int i=0;i<x;i++){
+            arr[i]=sc.nextInt();
+        }
+        for(int i=0;i<x;i++){
+            for(int j=i+1;j<x;j++){
+                if(arr[i]>arr[j]){
+               int temp=arr[i];
+               arr[i]=arr[j];
+               arr[j]=temp;
+                }
+            }
+          
+        }
+          System.out.println(arr[0]-1);
+    }
+}
